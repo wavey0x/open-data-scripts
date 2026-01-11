168519df03884c3a5ec1612ceee7b96b70fa857a
wavey0x <wavey0x@proton.me>
2026-01-11 15:13:10 -0500

    update runner

--- scripts/run.py
+++ scripts/run.py
@@ -6,6 +6,7 @@ warnings.filterwarnings("ignore", category=UserWarning, module="web3")
 from scripts.ybs_dash import main as ybs_job
 from scripts.ybs_dash.listeners import event_listener
 from scripts.resupply import main as resupply_job
+from scripts.resupply import position_monitor
 import os, subprocess, datetime
 import shutil
 from config import (
@@ -17,11 +18,15 @@ from config import (
 
 def main():
     resupply_job.main()
-    ybs_job.main()
-    event_listener.main()
-
     destination_dir = '../open-data/'
     os.makedirs(destination_dir, exist_ok=True)
+
+    chart_dir = os.path.join(destination_dir, "charts")
+    chart_path = os.path.join(chart_dir, "resupply_positions.png")
+    meta_path = os.path.join(chart_dir, "resupply_positions_meta.json")
+    position_monitor.main(output_path=chart_path, meta_path=meta_path)
+    ybs_job.main()
+    event_listener.main()
 
     source_files = [
         get_json_path(YBS_JSON_FILE),
@@ -43,6 +48,8 @@ def push_to_gh(project_directory):
     github_repo = 'github.com/wavey0x/open-data.git'
     github_token = os.getenv('GITHUB_PAT')
     remote_url = f'https://{github_token}@{github_repo}'
+    bot_name = os.getenv("BOT_GIT_NAME", "wavey0x-bot")
+    bot_email = os.getenv("BOT_GIT_EMAIL", "wavey0x-bot@proton.me")
     os.chdir(project_directory)
 
     try:
@@ -56,6 +63,10 @@ def push_to_gh(project_directory):
         subprocess.run(['git', 'add', '-A'], check=True)
 
         # Commit the changes
+        os.environ['GIT_AUTHOR_NAME'] = bot_name
+        os.environ['GIT_AUTHOR_EMAIL'] = bot_email
+        os.environ['GIT_COMMITTER_NAME'] = bot_name
+        os.environ['GIT_COMMITTER_EMAIL'] = bot_email
         print(f'Remote URL: {remote_url}')
         current_datetime = datetime.datetime.now()
         formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
