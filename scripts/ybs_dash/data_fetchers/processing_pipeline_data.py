56183cead133794b90e3fb73241d3eca6e8e8a6a
wavey0x <wavey0x@proton.me>
2026-06-09 10:22:20 -0400

    Avoid explorer lookups for fee burner tokens

--- scripts/ybs_dash/data_fetchers/processing_pipeline_data.py
+++ scripts/ybs_dash/data_fetchers/processing_pipeline_data.py
@@ -1,6 +1,38 @@
-from brownie import Contract, ZERO_ADDRESS, chain
-import utils as utilities
+import logging
+
+from brownie import Contract
 from constants import YCRV_SPLITTER, YCRV_RECEIVER, YCRV_FEE_BURNER, TRADE_HANDLER, YCRV, CRVUSD
+
+log = logging.getLogger(__name__)
+
+ERC20_METADATA_ABI = [
+    {
+        "name": "balanceOf",
+        "type": "function",
+        "stateMutability": "view",
+        "inputs": [{"name": "account", "type": "address"}],
+        "outputs": [{"name": "", "type": "uint256"}],
+    },
+    {
+        "name": "decimals",
+        "type": "function",
+        "stateMutability": "view",
+        "inputs": [],
+        "outputs": [{"name": "", "type": "uint8"}],
+    },
+    {
+        "name": "symbol",
+        "type": "function",
+        "stateMutability": "view",
+        "inputs": [],
+        "outputs": [{"name": "", "type": "string"}],
+    },
+]
+
+
+def erc20_metadata_contract(address):
+    return Contract.from_abi("ERC20Metadata", address, ERC20_METADATA_ABI)
+
 
 def build_data(token, staker_data):
     if token != YCRV:
@@ -13,13 +45,33 @@ def build_data(token, staker_data):
         burn_tokens.append(CRVUSD)
         
     balances = {}
-    for token in burn_tokens:
-        token = Contract(token)
-        balance = token.balanceOf(YCRV_FEE_BURNER)
-        if balance > 1:
-            balances[token.address] = {}
-            balances[token.address]['balance'] = balance / 10 ** token.decimals()
-            balances[token.address]['symbol'] = token.symbol()
+    for token_address in burn_tokens:
+        token = erc20_metadata_contract(token_address)
+        try:
+            balance = token.balanceOf(YCRV_FEE_BURNER)
+        except Exception as exc:
+            log.warning("Skipping fee burner token %s: balanceOf failed: %s", token_address, exc)
+            continue
+
+        if balance <= 1:
+            continue
+
+        try:
+            decimals = token.decimals()
+        except Exception as exc:
+            log.warning("Skipping fee burner token %s: decimals failed: %s", token_address, exc)
+            continue
+
+        try:
+            symbol = token.symbol()
+        except Exception as exc:
+            log.warning("Using address as symbol for fee burner token %s: %s", token_address, exc)
+            symbol = token.address
+
+        balances[token.address] = {
+            'balance': balance / 10 ** decimals,
+            'symbol': symbol,
+        }
 
         
     reward_token = staker_data['reward_token']
@@ -39,4 +91,3 @@ def build_data(token, staker_data):
         'split_ratio_admin_fees': admin_splits,
         'split_ratio_vote_incentives': incentive_splits,
     }
-
